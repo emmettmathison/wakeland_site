c3cdcc724fa02022b5d9471554aa60fe47dc5ce6
Crash Override <emmettmathison@gmail.com>
2021-02-20 21:00:12 -0800

    resize

--- routes/HTML.js
+++ routes/HTML.js
@@ -1,6 +1,12 @@
 var path = require("path");
 
 module.exports = function (app) {
+  app.get("/phone.html", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/html/phone.html"));
+  });
+  app.get("/home.html", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/html/home.html"));
+  });
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../client/html/home.html"));
   });
